a6ae5b17a98286a4eaff0d01d887e531bb39e307
Mahesh Sabnis <sabnis_m@hotmail.com>
2023-04-27 11:40:24 +0530

    Add files via upload

--- DML.sql
+++ DML.sql
@@ -1,35 +1,44 @@
--- INsert
+use eShoppingCodi;
 
-insert into Department Values(60, 'Admin', 'Mumbai', 100); 
--- Defining Parameters
-declare @DeptNo int;
-Set @DeptNo = 70;
-declare @DeptName varchar(200);
-Set @DeptName = 'Training';
-declare @Location varchar(200);
-Set @Location = 'Pune';
-declare @Capacity int;
-Set @Capacity = 900;
+insert into Category Values(1001, 'Electronics', 2000);
 
-Insert into Department(DeptNo, DeptName, Location, Capacity) 
-values
-(@DeptNo,@DeptName,@Location,@Capacity);
+insert into Category Values(1002, 'Electrical', 20);
+insert into Category Values(1003, 'Food', 60);
+insert into Category Values(1004, 'Fashion', 200);
+
+Select * from Category;
+
+-- Lets Update
+Update Category SET CategoryName='Electronics-IT' where CategoryId=1001;
+
+-- Lets Delete
+Delete Category where CategoryId=1004;
+
+-- Now for Product
+Insert into Product Values('Prod-001','Laptop', '64 GB RAM 1 TB SSD', 234000, 1001, 'Apple');
+
+Insert into Product Values('Prod-002','Laptop', '32 GB RAM 2 TB SSD', 220000, 1001, 'Acer');
 
 
-Select * from Employee where DeptNo=10;
+select * from Product;
 
-Update Employee Set Salary = Salary + 10000
-where DeptNo = 10 and Designation = 'Director';
+Delete Product where ProductUniqueId=3;
 
-select * from Department;
+-- Truncate the Table by Resetting it and deleting all Roecords
 
-Delete From Department where DeptNo=70;
+truncate Table Product;
 
-Delete from Employee
-where DeptNo in (40,50);
 
-select * from Department;
-Select * from Employee;
+Insert into Product Values('Prod-003','RAM', '64 GB', 34000, 1001, 'HP');
+
+Insert into Product Values('Prod-004','DVD Writter', 'Fast Writer', 4000, 1001, 'Nero');
+
+Insert into Product Values('Prod-005','Mixer', 'Mix Anything', 3000, 1002, 'Bajaj');
+
+Insert into Product Values('Prod-006','Iron', 'Press All', 5000, 1002, 'Phillipse');
+
+
+
 
 
 
---
+++ SP_Table.sql
@@ -0,0 +1,15 @@
+create procedure p_x
+as
+begin
+insert @t values('a', 1,1,1)
+insert @t values('b', 2,2,2)
+
+select * from @t
+end
+go
+
+declare @t table(col1 varchar(10), col2 float, col3 float, col4 float)
+insert @t
+exec p_x
+
+select * from @t
